04d3050e11d954d64c489e19d1b4a5fbd0f078e4
Kundan Kumar <sivagroup.kundan@gmail.com>
2024-12-21 12:19:00 +0530

    kundan updates
    updated product page

--- src/app/(pages)/rfid-tags-and-labels/[id]/page.js
+++ src/app/(pages)/rfid-tags-and-labels/[id]/page.js
@@ -1,17 +1,77 @@
 import { Box, Typography } from "@mui/material";
+import * as APIURLS from "@/apis/apiconstant";
+import * as FETCHAPI from "@/apis/fetchapi";
+
+async function GetProductDetailsByUrlName(url) {
+  let data;
+  const reqData = { UrlName: url };
+  try {
+    let res = await FETCHAPI.Fetch(
+      APIURLS.APIURL.ProductDetailsByUrlName,
+      reqData
+    );
+    if (res.status === 200) {
+      data = await res.json();
+    }
+  } catch (ex) {}
+  return data;
+}
 
 export async function generateMetadata({ params }) {
-  const ID = (await params).id;
+  const url = (await params).id;
+  const ProductData = await GetProductDetailsByUrlName(url);
+  const { MetaTitle, MetaDescription, ProductImage, Title } = ProductData;
   return {
-    title: "Product ID : " + ID,
+    title: MetaTitle,
+    description: MetaDescription,
+    robots: "index, follow", // Default is to allow indexing
+
+    // Open Graph (OG) Metadata
+    openGraph: {
+      type: "website",
+      locale: "en_US",
+      url: `https://sivaiot.co/rfid-tags-and-labels/${url}`,
+      title: MetaTitle,
+      description: MetaDescription,
+      images: [
+        {
+          url: `https://sivafiles.sivagroup.co/1/images/productpage/${ProductImage}`,
+          width: 1200,
+          height: 630,
+          alt: Title,
+        },
+      ],
+    },
+
+    // Twitter Metadata
+    twitter: {
+      card: "summary_large_image",
+      site: "@sivaiot.co",
+      creator: "@sivaiot.co",
+      title: MetaTitle,
+      description: MetaDescription,
+      images: [
+        `https://sivafiles.sivagroup.co/1/images/productpage/${ProductImage}`,
+      ],
+    },
+
+    // Canonical URL
+    alternates: {
+      canonical: `https://sivaiot.co/rfid-tags-and-labels/${url}`,
+    },
+
+    // Additional Metadata
+    charset: "UTF-8",
   };
 }
 
 const TestingPage = async ({ params }) => {
-  const ID = (await params).id;
+  const url = (await params).id;
+  const ProductData = await GetProductDetailsByUrlName(url);
+
   return (
     <Box sx={{ textAlign: "center", height: 400 }}>
-      <Typography variant="h4">Product ID : {ID}</Typography>
+      <Typography variant="h4">Product url : {url}</Typography>
     </Box>
   );
 };
--- src/app/(pages)/rfid-tags-and-labels/page.js
+++ src/app/(pages)/rfid-tags-and-labels/page.js
@@ -1,4 +1,7 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Container, Divider, Typography } from "@mui/material";
+import * as APIURLS from "@/apis/apiconstant";
+import * as FETCHAPI from "@/apis/fetchapi";
+import Link from "next/link";
 
 export const metadata = {
   title: "RFID Tag Manufacturer | RFID Label Supplier - SIVA IoT",
@@ -6,10 +9,53 @@ export const metadata = {
     "SIVA IoT develops and manufactures wireless identification RFID labels & tags that identifies assets, streamlines critical production processes, makes payments secure, supply chains efficient and much more. More than just RFID labels",
 };
 
-const RFIDProducts = () => {
+async function GetAllCategoriesWithProduct() {
+  let data;
+  try {
+    let res = await FETCHAPI.Fetch(APIURLS.APIURL.WebCategoryWiseProducts);
+    if (res.status === 200) {
+      data = await res.json();
+    }
+  } catch (ex) {}
+  //   console.log("data>>>", data);
+  //   console.log("data>>>productList", data[0].productList[0]);
+  return data;
+}
+
+const RFIDProducts = async () => {
+  const Categories = await GetAllCategoriesWithProduct();
+  if (!Categories) return;
   return (
-    <Box sx={{ height: 400, textAlign: "center" }}>
-      <Typography variant="h4">RFID Product page</Typography>
+    <Box>
+      <Typography
+        variant="h4"
+        sx={{ textAlign: "center" }}
+      >
+        RFID Product page
+      </Typography>
+      <Container>
+        {Categories.map((category, index) => (
+          <Box
+            key={index}
+            sx={{
+              margin: 2,
+              padding: 2,
+              border: 1,
+              borderRadius: 2,
+            }}
+          >
+            <Typography variant="h6">{category.name}</Typography>
+            <Divider />
+            {category?.productList?.map((product, i) => (
+              <Box key={i}>
+                <Link href={"/rfid-tags-and-labels/" + product.link}>
+                  {product.name}
+                </Link>
+              </Box>
+            ))}
+          </Box>
+        ))}
+      </Container>
     </Box>
   );
 };
