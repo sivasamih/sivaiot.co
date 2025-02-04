9155f557a04f858eee3caeae106ebe49539e2853
Kundan Kumar <sivagroup.kundan@gmail.com>
2025-02-04 12:34:53 +0530

    kundan updates
    added a redirection link

--- src/components/compo/redirection/redirection.js
+++ src/components/compo/redirection/redirection.js
@@ -4,6 +4,8 @@ import { Route_Path } from "@/apis/api";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 
+const RedirectionList = [{ from: "/rfid-product-families/on-metal-rfid-labels", to: "/rfid-product-families/on-metal-uhf-labels" }];
+
 const RedirectionPage = () => {
   const PathName = usePathname();
   const searchParams = useSearchParams();
@@ -13,18 +15,20 @@ const RedirectionPage = () => {
     const name = searchParams.get("name");
 
     try {
-      if (
-        PathName === Route_Path.PRODUCTS_DETAILS ||
-        (PathName === Route_Path.PRODUCTS && name)
-      ) {
+      const redirection = RedirectionList ? RedirectionList.find((item) => item.from === PathName) : null;
+      if (redirection) {
+        router.push(redirection.to);
+        return;
+      }
+    } catch (ex) { }
+
+    try {
+      if (PathName === Route_Path.PRODUCTS_DETAILS || (PathName === Route_Path.PRODUCTS && name)) {
         const NewPath = Route_Path.PRODUCTS + "/" + name;
         router.push(NewPath);
       }
 
-      if (
-        PathName === Route_Path.PRODUCT_FAMILY_DETAILS ||
-        (PathName === Route_Path.PRODUCT_FAMILY && name)
-      ) {
+      if (PathName === Route_Path.PRODUCT_FAMILY_DETAILS || (PathName === Route_Path.PRODUCT_FAMILY && name)) {
         const NewPath = Route_Path.PRODUCT_FAMILY + "/" + name;
         router.push(NewPath);
       }
